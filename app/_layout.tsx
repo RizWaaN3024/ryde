554ff5ab8b7bf83cbb1f712ca998e508d164136f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-01 02:07:29 +0530

    Updated tailwind-config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,29 @@ import 'react-native-reanimated';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-  );
+    const [loaded] = useFonts({
+        "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+        "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+        "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+        "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    });
+
+    useEffect(() => {
+        if(loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded])
+    
+    if(!loaded) {
+        return null;
+    }    
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+        </Stack>
+    );
 }
