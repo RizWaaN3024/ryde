f944d65b106bcc20e912fb0ddfacff55c76b9b08
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-01 02:19:36 +0530

    Folder setup

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Rides = () => {
+  return (
+    <View>
+      <Text>Rides</Text>
+    </View>
+  )
+}
+
+export default Rides
