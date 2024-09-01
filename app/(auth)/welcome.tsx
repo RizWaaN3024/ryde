c27eaa97f215325ebf06e31a8c893043bb945c13
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-02 00:56:59 +0530

    Added welcome Screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React, { useRef, useState } from 'react'
 import { router } from 'expo-router'
 import Swiper from "react-native-swiper";
@@ -26,8 +26,20 @@ const Onboarding = () => {
             onIndexChanged={(index) => setActiveIndex(index)}
         >
             {onboarding.map((item) => (
-                <View key={item.id}>
-                    <Text>{item.title}</Text>
+                <View key={item.id} className='flex items-center justify-center p-5'>
+                    <Image 
+                        source={item.image}
+                        className='w-full h-[300px]'
+                        resizeMode='contain'
+                    />
+                    <View className='flex flex-row items-center justify-center w-full mt-10'>
+                        <Text className='text-black text-3xl font-bold mx-10 text-center'>
+                            {item.title}
+                        </Text>
+                    </View>
+                    <Text className='text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>
+                        {item.description}
+                    </Text>
                 </View>
             ))}
         </Swiper>
