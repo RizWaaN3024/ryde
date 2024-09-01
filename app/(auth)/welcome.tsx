167aaf151f6ab599138b77b15ab70fc7ae143620
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-02 00:49:53 +0530

    Added swiper and types declarations for images

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,8 +1,12 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { router } from 'expo-router'
+import Swiper from "react-native-swiper";
+import { onboarding } from '@/constants';
 
 const Onboarding = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className='flex h-full items-center justify-between bg-white'>
         <TouchableOpacity onPress={() => {
@@ -10,6 +14,23 @@ const Onboarding = () => {
         }} className='w-full flex justify-end items-end p-5'>
             <Text className='text-black text-md font-JakartaBold'>Skip</Text>
         </TouchableOpacity>
+        <Swiper 
+            ref={swiperRef}
+            loop={false}
+            dot={
+                <View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full' />
+            }
+            activeDot={
+                <View className='w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full' />
+            }
+            onIndexChanged={(index) => setActiveIndex(index)}
+        >
+            {onboarding.map((item) => (
+                <View key={item.id}>
+                    <Text>{item.title}</Text>
+                </View>
+            ))}
+        </Swiper>
     </SafeAreaView>
   )
 }
