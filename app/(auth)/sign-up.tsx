bef7375cdd2b3d64c9447f7266c1c2ca765372bf
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-03 02:29:03 +0530

    Created InputField COmpponent

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { icons, images } from '@/constants'
+import InputField from '@/components/InputField'
 
 const SignUp = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <ScrollView className='flex-1 bg-white'>
+      <View className='flex-1 bg-white'>
+        <View className='relative w-full h-[250px]'>
+          <Image source={images.signUpCar} className='z-0 w-full h-[250px]' />
+          <Text className='text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5'>Create Your Account</Text>
+        </View>
+        <View>
+          <InputField 
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({...form, name: value})}
+          />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
