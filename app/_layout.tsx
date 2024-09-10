d66c999d8d35335da912ff920536ec88c17d8a2c
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-11 02:15:55 +0530

    Authentication initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -18,6 +22,13 @@ export default function RootLayout() {
         "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
     });
 
+    if (!publishableKey) {
+        throw new Error(
+          'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+        )
+      }
+      
+
     useEffect(() => {
         if(loaded) {
             SplashScreen.hideAsync();
@@ -27,12 +38,18 @@ export default function RootLayout() {
     if(!loaded) {
         return null;
     }    
+
+    
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(root)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-        </Stack>
+        <ClerkProvider publishableKey={publishableKey}>
+            <ClerkLoaded>
+                <Stack>
+                    <Stack.Screen name="index" options={{ headerShown: false }} />
+                    <Stack.Screen name="(root)" options={{ headerShown: false }} />
+                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                    <Stack.Screen name="+not-found" />
+                </Stack>    
+            </ClerkLoaded>
+        </ClerkProvider>
     );
 }
---
+++ lib/auth.ts
@@ -0,0 +1,34 @@
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+
+
+export interface TokenCache {
+    getToken: (key: string) => Promise<string | undefined | null>
+    saveToken: (key: string, token: string) => Promise<void>
+    clearToken?: (key: string) => void
+  }
+
+  export const tokenCache = {
+    async getToken(key: string) {
+      try {
+        const item = await SecureStore.getItemAsync(key)
+        if (item) {
+          console.log(`${key} was used 🔐 \n`)
+        } else {
+          console.log('No values stored under key: ' + key)
+        }
+        return item
+      } catch (error) {
+        console.error('SecureStore get item error: ', error)
+        await SecureStore.deleteItemAsync(key)
+        return null
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value)
+      } catch (err) {
+        return
+      }
+    },
+  }
