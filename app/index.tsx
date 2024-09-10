f7cc03e2a7ab2e8bec9beb97b9df74a223fe5e7f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-11 02:18:15 +0530

    Authentication initial setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Redirect } from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
+
 
 const Home = () => {
+  const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href='/(root)/(tabs)/home' />
+  }
   return <Redirect href="/(auth)/welcome" />
 }
 
